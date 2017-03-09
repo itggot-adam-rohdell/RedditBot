e259984850bef3dccced56ba66596f3fbd9d2502
Adam Rohdell <adamrohdell@hotmail.com>
2017-03-09 16:57:37 +0100

    started working on the token, finished the authorization base

---
+++ RedditBot/RedditBot/RedditAccessToken.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RedditBot
+{
+    class RedditAccessToken
+    {
+        public string token{ get; private set; }
+        public string token_type;
+        public int expirationDateInSeconds;
+        private DateTime _timeOfCreation;
+
+
+        public RedditAccessToken(string inToken, string inToken_type, int timeToExpirationInSeconds)
+        {
+            _timeOfCreation = DateTime.Now;
+            token = inToken;
+            token_type = inToken_type;
+            expirationDateInSeconds = Convert.ToInt16(timeToExpirationInSeconds);
+        }
+
+        public int TimeLeftToRenew()
+        {
+            return expirationDateInSeconds - Convert.ToInt16((DateTime.Now - _timeOfCreation).TotalSeconds);
+        }
+    }
+}
--- RedditBot/RedditBot/RedditBot.cs
+++ RedditBot/RedditBot/RedditBot.cs
@@ -3,10 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace RedditBot
 {
     class RedditBot
     {
+        static RedditBot()
+        {
+
+        }
+
+        public static RedditAccessToken (string username, string password)
+        {
+
+            using (var client = new HttpClient())
+            {
+                string clientId = "r11-_6UcVOcikg";
+                string clientSecret = "dJAaEzTDmNoxXU5j3_hk1ks9DQQ";
+                var authenticationArray = Encoding.ASCII.GetBytes($"{clientId}:{clientSecret}");
+                var encodedAuthenticationString = Convert.ToBase64String(authenticationArray);
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", encodedAuthenticationString);
+
+                var clientVersion = "0.01";
+                var redditUsername = "PrettyNiceBotTest";
+                var redditPassword = "BotTest?";
+                client.DefaultRequestHeaders.Add("User-Agent", $"NiceBotTest /v{clientVersion} by {redditUsername}");
+
+                var formData = new Dictionary<string, string>
+                {
+                    { "grant_type", "password" },
+                    { "username", redditUsername },
+                    { "password", redditPassword }
+                };
+                var encodedFormData = new FormUrlEncodedContent(formData);
+                var authUrl = "https://www.reddit.com/api/v1/access_token";
+                var response = client.PostAsync(authUrl, encodedFormData).GetAwaiter().GetResult();
+
+                // Response Code
+                Console.WriteLine(response.StatusCode);
+
+                // Actual Token
+                var responseData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                Console.WriteLine(responseData);
+                var accessToken = JObject.Parse(responseData).SelectToken("access_token").ToString();
+
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("bearer", accessToken);
+
+                
+                responseData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                RedditAccessToken token = new RedditAccessToken(accessToken, JObject.Parse(responseData).SelectToken("token_type").ToString(), Convert.ToInt16(JObject.Parse(responseData).SelectToken("expires_in")));
+                Console.ReadKey();
+            }
+    }
     }
 }
